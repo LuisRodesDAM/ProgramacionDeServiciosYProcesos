7c0095e402735017df0dbdbc8ac0dcd8e0cde82a
LuisRodesDam <luirodroc@alu.edu.gva.es>
2025-01-15 13:19:56 +0100

    intell

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el primer numero: ");
+        int num = sc.nextInt();
+        System.out.print("Ingrese el segundo numero: ");
+        int num2 = sc.nextInt();
+        System.out.println("numeros  " + num + num2);
 
     }
 }
