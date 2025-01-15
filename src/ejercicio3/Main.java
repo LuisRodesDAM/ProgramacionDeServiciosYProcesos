aef4c6fddefe199d722a1f5d9aa928733ef6f517
LuisRodesDam <luirodroc@alu.edu.gva.es>
2025-01-15 19:09:32 +0100

    desde consola git bash

--- src/ejercicio3/Main.java
+++ src/ejercicio3/Main.java
@@ -1,17 +1,35 @@
 package src.ejercicio3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    private static ArrayList<Thread> hilos = new ArrayList<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce el numero de hilos: ");
+        System.out.print("Introduce el numero de hilos: ");
         int numeroHilos = sc.nextInt();
         for (int i = 1; i <= numeroHilos; i++) {
             Primos bp = new Primos();
             Thread hilo = new Thread(bp);
+            hilos.add(hilo);
             hilo.setName("Hilo " + i);
             hilo.start();
+        }
+        boolean quedanHilos = true;
+        while (quedanHilos) {
+            quedanHilos = false;
+            for(Thread hilo : hilos) {
+                System.out.println(hilo.threadId() + " "  +hilo.getName() + " "+ hilo.getState());
+                if(hilo.isAlive()) {
+                    quedanHilos = true;
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
---
+++ src/ejercicio4/Main.java
@@ -0,0 +1,10 @@
+package src.ejercicio4;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        System.out.println(rand.nextInt(10, 20));
+    }
+}
